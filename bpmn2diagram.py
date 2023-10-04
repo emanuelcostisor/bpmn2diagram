f044e05f3ebc568711e818ef27060ad37c39f440
Emanuel Costisor <emanuel.costisor@onewelcome.com>
2023-10-04 10:44:27 +0300

    using __name__=='__main__'

--- bpmn2diagram.py
+++ bpmn2diagram.py
@@ -14,28 +14,6 @@ from diagrams.azure.compute import CloudServices
 from diagrams.azure.ml import MachineLearningStudioWebServicePlans
 from diagrams.onprem.database import Mongodb
 
-parser = argparse.ArgumentParser()
-parser.add_argument('bpmn')
-parser.add_argument('direction', nargs='?', default='TB')
-args = parser.parse_args()
-
-args.direction = args.direction.upper()
-if args.direction not in ['TB', 'BT', 'LR', 'RL']:
-    print(
-        f'Invalid direction param {args.direction} reverting to default, "TB"')
-    args.direction = 'TB'
-
-tree = None
-
-with open(args.bpmn) as file:
-    tree = ET.parse(file)
-
-root = tree.getroot()
-name = root[0].attrib['id']
-
-path = {}
-
-
 def icon(element):
     if 'serviceTask' in element.tag:
         if '{http://activiti.org/bpmn}delegateExpression' in element.attrib:
@@ -58,25 +36,46 @@ def icon(element):
         return MachineLearningStudioWebServicePlans(element.attrib['id'])
     else:
         return DataLakeAnalytics(element.attrib['id'])
+    
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bpmn')
+    parser.add_argument('direction', nargs='?', default='TB')
+    args = parser.parse_args()
 
+    args.direction = args.direction.upper()
+    if args.direction not in ['TB', 'BT', 'LR', 'RL']:
+        print(
+            f'Invalid direction param {args.direction} reverting to default, "TB"')
+        args.direction = 'TB'
 
-with Diagram(name, direction=args.direction):
-    for element in root[0]:
-        if 'id' in element.attrib and 'sequenceFlow' not in element.tag:
-            if element.attrib['id'] not in path:
-                path[element.attrib['id']] = {}
-                path[element.attrib['id']]['node'] = icon(element)
-                path[element.attrib['id']]['target'] = []
-                if 'attachedToRef' in element.attrib:
-                    path[element.attrib['attachedToRef']]['target'].append(
-                        element.attrib['id'])
-    for element in root[0]:
-        if 'sourceRef' in element.attrib:
-            path[element.attrib['sourceRef']]['target'].append(
-                element.attrib['targetRef'])
-    for node in path:
-        for target in path[node]['target']:
-            try:
-                path[node]['node'] >> path[target]['node']
-            except Exception as e:
-                print(e)
+    tree = None
+
+    with open(args.bpmn) as file:
+        tree = ET.parse(file)
+
+    root = tree.getroot()
+    name = root[0].attrib['id']
+
+    path = {}
+
+    with Diagram(name, direction=args.direction):
+        for element in root[0]:
+            if 'id' in element.attrib and 'sequenceFlow' not in element.tag:
+                if element.attrib['id'] not in path:
+                    path[element.attrib['id']] = {}
+                    path[element.attrib['id']]['node'] = icon(element)
+                    path[element.attrib['id']]['target'] = []
+                    if 'attachedToRef' in element.attrib:
+                        path[element.attrib['attachedToRef']]['target'].append(
+                            element.attrib['id'])
+        for element in root[0]:
+            if 'sourceRef' in element.attrib:
+                path[element.attrib['sourceRef']]['target'].append(
+                    element.attrib['targetRef'])
+        for node in path:
+            for target in path[node]['target']:
+                try:
+                    path[node]['node'] >> path[target]['node']
+                except Exception as e:
+                    print(e)
